1d912882f1853700af68feace9e28a2efe76e7b5
Imran Mir <imran@intelligrape.com>
2016-02-08 11:05:37 +0530

    Addd first unit test file

--- test/com/im/test/UserSpec.groovy
+++ test/com/im/test/UserSpec.groovy
@@ -3,4 +3,37 @@ package com.im.test
 import spock.lang.Specification
 
 class UserSpec extends Specification {
+
+           def "First test"(){
+               expect:
+               true
+           }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
